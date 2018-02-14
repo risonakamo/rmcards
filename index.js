00c9950fb92ed10465e1eb33d569962e9dd802c0
risonakamo <hmykanae@gmail.com>
2018-02-14 14:49:47 -0500

    cardhandler object info hide label initial click action

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 window.onload=main;
 
+class cardHandler
+{
+    constructor()
+    {
+        var doc=document;
+        this.displayImg=doc.querySelector(".img-zone img");
+
+        var infoZone=doc.querySelector(".info-zone");
+        this.infoBoxes=infoZone.querySelectorAll(".info-box");
+        this.infoTexts=infoZone.querySelectorAll(".info-text");
+        this.infoButtons=infoZone.querySelectorAll(".hide-label");
+        this.initinfoButtons();
+    }
+
+    initinfoButtons()
+    {
+        this.infoButtons.forEach((i,x)=>{
+            i.addEventListener("click",(e)=>{
+                this.infoBoxes[x].classList.add("show");
+            });
+        });
+    }
+}
+
+var cardhandler;
+
 function main()
 {
-
+    var cardhandler=new cardHandler();
 }
